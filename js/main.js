c5737e008fd791d481fec9c77e75a3971a6f93e7
Jacob Bodkin <bodkin.j@gmail.com>
2017-03-16 15:57:26 -0500

    added dealer and outcome logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 console.log('JS is working!');
 $(function() {
-
+  $('#playerOneHit').hide();
+  $('#playerOneStand').hide();
 
 // Fisher-Yates Shuffle
 // http://stackoverflow.com/a/6274398
@@ -45,34 +46,72 @@ $(function() {
 var handDealer=[];
 var handPlayerOne=[];
 var $card=$('#deckCard');
+var wins=0;
+var losses=0;
+var pushes=0;
+
+
+
+
+
 
 //deal the cards
 $('#dealButton').click(function() {
   $('.cardArea').empty();
+  $('#playerOneHit').show();
+  $('#playerOneStand').show();
   handDealer=[];
   handPlayerOne=[];
   //check how much of deck is left
   if(shuffledCards.length<20) {
     deck_of_cards();
   };
-  for (var i=0;i<2;i++) {
+  //first card for each
     handPlayerOne.push(shuffledCards[0]);
     shuffledCards.shift();
     $card.clone().css('display','inline-block').appendTo('#playerOneCards');
-    $('#playerOneCards h3.suit').eq(i).text(handPlayerOne[i].suit);
-    $('#playerOneCards h3.value').eq(i).text(handPlayerOne[i].value);
-    // if (handPlayerOne.length===2) {
-    //   $('#playerOneCards div.card').eq(1).css('z-index', '1').css('left','-3em');
-    // };
+    $('#playerOneCards h3.suit').eq(0).text(handPlayerOne[0].suit);
+    $('#playerOneCards h3.value').eq(0).text(handPlayerOne[0].value);
+    handDealer.push(shuffledCards[0]);
+    shuffledCards.shift();
+    $card.clone().css('display','inline-block').appendTo('#dealerCards').attr('id', 'dealerFirstCard');
+    $('#dealerCards h3.suit').eq(0).text(handDealer[0].suit).hide();
+    $('#dealerCards h3.value').eq(0).text(handDealer[0].value).hide();
+    $('#dealerFirstCard').eq(0).css('background-image', 'url(img/card-back-red.png)');
+
+
+  //second card for each
+    handPlayerOne.push(shuffledCards[0]);
+    shuffledCards.shift();
+    $card.clone().css('display','inline-block').appendTo('#playerOneCards');
+    $('#playerOneCards h3.suit').eq(1).text(handPlayerOne[1].suit);
+    $('#playerOneCards h3.value').eq(1).text(handPlayerOne[1].value);
     handDealer.push(shuffledCards[0]);
     shuffledCards.shift();
     $card.clone().css('display','inline-block').appendTo('#dealerCards');
-    $('#dealerCards h3.suit').eq(i).text(handDealer[i].suit);
-    $('#dealerCards h3.value').eq(i).text(handDealer[i].value);
-    // if (handDealer.length===2) {
-    //   $('#dealerCards div.card').eq(1).css('z-index', '1').css('left','-3em');
-    // };
-  };
+    $('#dealerCards h3.suit').eq(1).text(handDealer[1].suit);
+    $('#dealerCards h3.value').eq(1).text(handDealer[1].value);
+
+  //check for blackjack
+  addPlayerOneTotal();
+  addDealerTotal();
+  if (handPlayerOneValue===21 && handDealerValue===21) {
+    $('#playerOneHit').hide();
+    $('#playerOneStand').hide();
+    pushes +=1;
+    $('#pushesText').text(pushes);
+  } else if (handDealerValue===21) {
+    $('#playerOneHit').hide();
+    $('#playerOneStand').hide();
+    losses+=1;
+    $('#lossesText').text(losses);
+  } else if (handPlayerOneValue===21) {
+    $('#playerOneHit').hide();
+    $('#playerOneStand').hide();
+    wins+=1;
+    $('#winsText').text(wins);
+  }
+
 });
 
 
@@ -81,19 +120,134 @@ $('#dealButton').click(function() {
 //hit-me, add card to player
 $('#playerOneHit').click(function() {
   handPlayerOne.push(shuffledCards[0]);
+  shuffledCards.shift();
   $card.clone().css('display','inline-block').appendTo('#playerOneCards');
   $('#playerOneCards h3.suit').eq(handPlayerOne.length-1).text(handPlayerOne[handPlayerOne.length-1].suit);
   $('#playerOneCards h3.value').eq(handPlayerOne.length-1).text(handPlayerOne[handPlayerOne.length-1].value);
+
+  //check for bust
+  addPlayerOneTotal();
+  if (handPlayerOneValue>21) {
+    $('#playerOneHit').hide();
+    $('#playerOneStand').hide();
+    losses+=1;
+    $('#lossesText').text(losses);
+  };
+
 });
 
+//end the game
+var handPlayerOneValue=0;
+var handDealerValue=0;
+$('#playerOneStand').click(function() {
+  addPlayerOneTotal();
+  $('#playerOneHit').hide();
+  $('#playerOneStand').hide();
+  $('#dealerFirstCard').css('background-image', 'none');
+  $('#dealerCards h3.suit').eq(0).show();
+  $('#dealerCards h3.value').eq(0).show();
+  console.log("Player: "+handPlayerOneValue);
+
+  //dealer's turn
+  addDealerTotal();
+  if (handDealerValue<17) {
+    addCardDealer();
+    addDealerTotal();
+  };
+
+//check for the outcome
+  addPlayerOneTotal();
+  addDealerTotal();
+  if (handDealerValue>21) {
+    wins+=1;
+    $('#winsText').text(wins);
+  } else if (handPlayerOneValue==handDealerValue) {
+    pushes +=1;
+    $('#pushesText').text(pushes);
+  } else if (handDealerValue>handPlayerOneValue) {
+    losses+=1;
+    $('#lossesText').text(losses);
+  } else if (handDealerValue<handPlayerOneValue) {
+    wins+=1;
+    $('#winsText').text(wins);
+  }
 
 
 
 
+});
 
+//add card to dealer function
+function addCardDealer() {
+  handDealer.push(shuffledCards[0]);
+  shuffledCards.shift();
+  $card.clone().css('display','inline-block').appendTo('#dealerCards');
+  $('#dealerCards h3.suit').eq(handDealer.length-1).text(handDealer[handDealer.length-1].suit);
+  $('#dealerCards h3.value').eq(handDealer.length-1).text(handDealer[handDealer.length-1].value);
+};
+//adding up Player One
+function addPlayerOneTotal() {
+  handPlayerOneValue=0;
+  var acePresentPlayerOne=false;
+  for (var i=0;i<handPlayerOne.length;i++) {
+    if (handPlayerOne[i].value=='Jack' || handPlayerOne[i].value=='Queen' || handPlayerOne[i].value=='King') {
+      handPlayerOneValue+=10;
+    } else if (handPlayerOne[i].value=='Ace' && 11+handPlayerOneValue>21) {
+      handPlayerOneValue+=1;
+      acePresentPlayerOne=true;
+    } else if (handPlayerOne[i].value=='Ace') {
+      handPlayerOneValue+=11;
+      acePresentPlayerOne=true;
+    } else {
+      handPlayerOneValue+=handPlayerOne[i].value;
+    };
+  };
+  if (acePresentPlayerOne==true && handPlayerOneValue>21) {
+    handPlayerOneValue=handPlayerOneValue-10;
+  };
+};
 
+//Adding up Dealer
+function addDealerTotal() {
+  var acePresentDealer=false;
+  handDealerValue=0;
+  for (var i=0; i<handDealer.length;i++) {
+    if (handDealer[i].value=='Jack' || handDealer[i].value=='Queen' ||  handDealer[i].value=='King') {
+      handDealerValue+=10;
+    } else if (handDealer[i].value=='Ace' && 11+handDealerValue>21) {
+      handDealerValue+=1;
+      acePresentDealer=true;
+    } else if (handDealer[i].value=='Ace') {
+      handDealerValue+=11;
+      acePresentDealer=true;
+    } else {
+      handDealerValue+=handDealer[i].value;
+    };
+  };
+  if (acePresentDealer==true && handDealerValue>21) {
+    handDealerValue=handDealerValue-10;
+  };
+  console.log("Dealer: "+handDealerValue);
+};
 
-
+$('#dealButton').mouseover(function () {
+  $('#dealButton').css('background-color', 'white');
+});
+$('#dealButton').mouseleave(function () {
+  $('#dealButton').css('background-color', 'rgba(0,129,10,1)');
+});
+$('#playerOneHit').mouseover(function () {
+  $('#playerOneHit').css('background-color', 'white');
+});
+$('#playerOneHit').mouseleave(function () {
+  $('#playerOneHit').css('background-color', 'rgba(0,129,10,1)');
+});
+$('#playerOneStand').mouseover(function () {
+  $('#playerOneStand').css('background-color', 'white');
+});
+$('#playerOneStand').mouseleave(function () {
+  $('#playerOneStand').css('background-color', 'rgba(0,129,10,1)');
+});
 
 
 
